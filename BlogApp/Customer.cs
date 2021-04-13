9a6d709caebeb8a1afbcb246173f4d37be097714
Egor <79940875+Bareen86@users.noreply.github.com>
2021-04-14 01:41:38 +0300

    Create Customer.cs

---
+++ BlogApp/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp
+{
+    class Customer
+    {
+        public string Name;
+        public int OrderNumber;
+        public int Price;
+    }
+}
